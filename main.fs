cf036389968247dc364742ff378028fc0cd3310c
Charley Shattuck <cshattuck@surewest.net>
2022-05-08 17:22:14 -0700

    TX Bolt protocol added

--- main.fs
+++ main.fs
@@ -49,45 +49,76 @@ variable data 4 ramALLOT \ 6 bytes in all
 \ Gemini protocol to the data array
 : mark ( mask a)  data + dup >r c@ or r> c! ; 
 : Gemini ( n)  /data $80 #, data c!
-    dup $0100000 #, and if $40 #, 1 #, mark then drop \ S1
-    dup $0200000 #, and if $10 #, 1 #, mark then drop \ T
-    dup $0400000 #, and if $04 #, 1 #, mark then drop \ P
-    dup $0800000 #, and if $01 #, 1 #, mark then drop \ H
-    dup $1000000 #, and if $08 #, 2 #, mark then drop \ *
-    dup $0008000 #, and if $02 #, 3 #, mark then drop \ F
-    dup $0004000 #, and if $40 #, 4 #, mark then drop \ P
-    dup $0002000 #, and if $10 #, 4 #, mark then drop \ L
-    dup $0001000 #, and if $04 #, 4 #, mark then drop \ T
-    dup $0000100 #, and if $01 #, 4 #, mark then drop \ D
-    dup $0080000 #, and if $20 #, 1 #, mark then drop \ S2
-    dup $0040000 #, and if $08 #, 1 #, mark then drop \ K
-    dup $0020000 #, and if $02 #, 1 #, mark then drop \ W
-    dup $0010000 #, and if $40 #, 2 #, mark then drop \ R
-    dup $0000200 #, and if $04 #, 2 #, mark then drop \ *
-    dup $0000001 #, and if $01 #, 3 #, mark then drop \ R
-    dup $0000002 #, and if $20 #, 4 #, mark then drop \ B
-    dup $0000004 #, and if $08 #, 4 #, mark then drop \ G
-    dup $0000800 #, and if $02 #, 4 #, mark then drop \ S
-    dup $0000400 #, and if $01 #, 5 #, mark then drop \ Z
-    dup $0000008 #, and if $20 #, 2 #, mark then drop \ A
-    dup $0000010 #, and if $10 #, 2 #, mark then drop \ O 
-    dup $0000020 #, and if $40 #, 5 #, mark then drop \ #
-    dup $0000040 #, and if $08 #, 3 #, mark then drop \ E
-    dup $0000080 #, and if $04 #, 3 #, mark then drop \ U
+    dup $0100000 #, and if/ $40 #, 1 #, mark then \ S1
+    dup $0200000 #, and if/ $10 #, 1 #, mark then \ T
+    dup $0400000 #, and if/ $04 #, 1 #, mark then \ P
+    dup $0800000 #, and if/ $01 #, 1 #, mark then \ H
+    dup $1000000 #, and if/ $08 #, 2 #, mark then \ *
+    dup $0008000 #, and if/ $02 #, 3 #, mark then \ F
+    dup $0004000 #, and if/ $40 #, 4 #, mark then \ P
+    dup $0002000 #, and if/ $10 #, 4 #, mark then \ L
+    dup $0001000 #, and if/ $04 #, 4 #, mark then \ T
+    dup $0000100 #, and if/ $01 #, 4 #, mark then \ D
+    dup $0080000 #, and if/ $20 #, 1 #, mark then \ S2
+    dup $0040000 #, and if/ $08 #, 1 #, mark then \ K
+    dup $0020000 #, and if/ $02 #, 1 #, mark then \ W
+    dup $0010000 #, and if/ $40 #, 2 #, mark then \ R
+    dup $0000200 #, and if/ $04 #, 2 #, mark then \ *
+    dup $0000001 #, and if/ $01 #, 3 #, mark then \ R
+    dup $0000002 #, and if/ $20 #, 4 #, mark then \ B
+    dup $0000004 #, and if/ $08 #, 4 #, mark then \ G
+    dup $0000800 #, and if/ $02 #, 4 #, mark then \ S
+    dup $0000400 #, and if/ $01 #, 5 #, mark then \ Z
+    dup $0000008 #, and if/ $20 #, 2 #, mark then \ A
+    dup $0000010 #, and if/ $10 #, 2 #, mark then \ O 
+    dup $0000020 #, and if/ $40 #, 5 #, mark then \ #
+    dup $0000040 #, and if/ $08 #, 3 #, mark then \ E
+    dup $0000080 #, and if/ $04 #, 3 #, mark then \ U
     drop ; 
 
-\ TX Bolt
-
+\ TX Bolt protocol to the data array
+: bolt ( n)  /data
+    dup $0100000 #, and if/ $01 #, 0 #, mark then \ S1
+    dup $0080000 #, and if/ $01 #, 0 #, mark then \ S2
+    dup $0200000 #, and if/ $02 #, 0 #, mark then \ T
+    dup $0040000 #, and if/ $04 #, 0 #, mark then \ K
+    dup $0400000 #, and if/ $08 #, 0 #, mark then \ P
+    dup $0020000 #, and if/ $10 #, 0 #, mark then \ W
+    dup $0800000 #, and if/ $20 #, 0 #, mark then \ H
+    dup $0010000 #, and if/ $01 #, 1 #, mark then \ R
+    dup $0000008 #, and if/ $02 #, 1 #, mark then \ A
+    dup $0000010 #, and if/ $04 #, 1 #, mark then \ O 
+    dup $1000000 #, and if/ $08 #, 1 #, mark then \ *
+    dup $0000200 #, and if/ $08 #, 1 #, mark then \ *
+    dup $0000040 #, and if/ $10 #, 1 #, mark then \ E
+    dup $0000080 #, and if/ $20 #, 1 #, mark then \ U
+    dup $0008000 #, and if/ $01 #, 2 #, mark then \ F
+    dup $0000001 #, and if/ $02 #, 2 #, mark then \ R
+    dup $0004000 #, and if/ $04 #, 2 #, mark then \ P
+    dup $0000002 #, and if/ $08 #, 2 #, mark then \ B
+    dup $0002000 #, and if/ $10 #, 2 #, mark then \ L
+    dup $0000004 #, and if/ $20 #, 2 #, mark then \ G
+    dup $0001000 #, and if/ $01 #, 3 #, mark then \ T
+    dup $0000800 #, and if/ $02 #, 3 #, mark then \ S
+    dup $0000100 #, and if/ $04 #, 3 #, mark then \ D
+    dup $0000400 #, and if/ $08 #, 3 #, mark then \ Z
+    dup $0000020 #, and if/ $10 #, 3 #, mark then \ #
+    drop ; 
 
 \ A-Z
 
 
 \ vectored emit, for testing
-variable 'spit
-: spit  'spit @ execute ; 
-: >emit  ['] emit 'spit ! ; 
-: >hc.  ['] hc. 'spit ! ; 
-: send  data a! 5 #, for c@+ spit next ;
+wvariable 'spit  \ execution tokens are 16 bits
+: spit  'spit w@ execute ; 
+: >emit  ['] emit 'spit w! ; 
+: >hc.  ['] hc. 'spit w! ; 
+: send  data a! 5 #, for c@+ spit next ; \ Gemini
+: ?send  data a!  \ TX Bolt
+    c@+ if dup spit then drop
+    c@+ if dup $40 #, or spit then drop
+    c@+ if dup $80 #, or spit then drop
+    c@+ if $c0 #, or spit exit then spit ;
 
 \ NKRO keyboard mode
 cvariable former
@@ -129,7 +160,8 @@ cvariable former
     drop Keyboard.releaseAll ; 
 
 \ slider switch determines the protocol
-: choose  7 #, @pin if/ Gemini send exit then send-NKRO ;
+\ : choose  7 #, @pin if/ Gemini send exit then send-NKRO ;
+: choose  7 #, @pin if/ bolt ?send exit then send-NKRO ;
 : go  begin scan choose again
 
 turnkey decimal init Keyboard.begin
